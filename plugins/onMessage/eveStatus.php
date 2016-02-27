b5d5bc9cd16eb43f09bad91a36f065a700cf7bcd
Michael Karbowiak <karbowiak@gmail.com>
2016-02-27 17:02:34 +0100

    nothing means offline in tqStatus language.. silly CCP

--- plugins/onMessage/eveStatus.php
+++ plugins/onMessage/eveStatus.php
@@ -52,10 +52,10 @@ class eveStatus
 
             $crestData = json_decode(downloadData("https://public-crest.eveonline.com/"), true);
 
-            $tqStatus = $crestData["serviceStatus"]["eve"];
-            $tqOnline = $crestData["userCounts"]["eve"];
+            $tqStatus = isset($crestData["serviceStatus"]["eve"]) ? $crestData["serviceStatus"]["eve"] : "offline";
+            $tqOnline = (int) $crestData["userCounts"]["eve"];
 
-            $msg = "**TQ Status:** ***{$tqStatus}*** / ***{$tqOnline}*** users online.";
+            $msg = "**TQ Status:** {$tqStatus} with {$tqOnline} users online.";
             $this->logger->info("Sending eve status info to {$channelName} on {$guildName}");
             $this->discord->api("channel")->messages()->create($channelID, $msg);
         }
@@ -67,8 +67,8 @@ class eveStatus
     function information()
     {
         return array(
-            "name" => "evestatus",
-            "trigger" => array("!status"),
+            "name" => "tq",
+            "trigger" => array("!tq", "!status"),
             "information" => "Shows the current status of Tranquility"
         );
     }
