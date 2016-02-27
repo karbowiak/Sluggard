171932f6b7a9b2e24f968ed3da06df2ee15cb8bb
Michael Karbowiak <karbowiak@gmail.com>
2016-02-27 16:20:26 +0100

    Fix the TQ status timer, default to offline for tqStatus, and also string compare with !==

--- timers.php
+++ timers.php
@@ -3,12 +3,12 @@
 // Check if TQs status has changed
 $loop->addPeriodicTimer(30, function() use ($logger, $client, $discord, $config) {
     $crestData = json_decode(downloadData("https://public-crest.eveonline.com/"), true);
-    $tqStatus = $crestData["serviceStatus"]["eve"];
-    $tqOnline = $crestData["userCounts"]["eve"];
+    $tqStatus = isset($crestData["serviceStatus"]["eve"]) ? $crestData["serviceStatus"]["eve"] : "offline";
+    $tqOnline = (int) $crestData["userCounts"]["eve"];
 
     // Store the current status in the permanent cache
     $oldStatus = getPermCache("eveTQStatus");
-    if($tqStatus != $oldStatus) {
+    if($tqStatus !== $oldStatus) {
         $msg = "**New TQ Status:** ***{$tqStatus}*** / ***{$tqOnline}*** users online.";
         $logger->info("TQ Status changed from {$oldStatus} to {$tqStatus}");
         $discord->api("channel")->messages()->create($config["plugins"]["periodicTQStatus"]["channelID"], $msg);
