4e1d726c548cd641c68f193260f4ac01a847a221
Michael Karbowiak <karbowiak@gmail.com>
2016-02-29 23:49:24 +0100

    add about plugin

--- plugins/onMessage/about.php
+++ plugins/onMessage/about.php
@@ -77,10 +77,27 @@ class about {
         if(isset($data["trigger"])) {
             $channelName = $msgData->channel->name;
             $guildName = $msgData->guild->name;
+            $startTime = $this->app->startTime;
+            $time1 = new DateTime(date("Y-m-d H:i:s", $startTime));
+            $time2 = new DateTime(date("Y-m-d H:i:s"));
+            $interval = $time1->diff($time2);
+            $gitRevision = $this->app->gitrevision->getRevision();
 
+            $msg = "```Hello, i am Sluggard - i am a bot created for EVE Online related Discord servers.
+Also, i am the half-brother of Sovereign, atleast in the Blasto 7 movie..
 
-            //$this->log->info("Sending time info to {$channelName} on {$guildName}");
-            //$msgData->user->reply($msg);
+About Me:
+Author: Karbowiak (Discord ID: 118440839776174081)
+Library: discord-hypertext (PHP)
+Current version: " . $gitRevision["short"]. " (Last Update: " . $gitRevision["lastChangeDate"] . ")
+Github Repo: https://github.com/karbowiak/Sluggard
+
+Statistics:
+Uptime: " . $interval->y . " Year(s), " .$interval->m . " Month(s), " . $interval->d ." Days, ". $interval->h . " Hours, " . $interval->i." Minutes, ".$interval->s." seconds.
+Memory Usage: ~" . round(memory_get_usage() / 1024 / 1024, 3) . "MB```";
+
+           $this->log->info("Sending about info to {$channelName} on {$guildName}");
+           $msgData->user->reply($msg);
         }
     }
 
